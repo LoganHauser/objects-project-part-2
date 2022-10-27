e53f328b3d2bf79e2175f2a1f7e7550b335c10f7
LaGz4643 <89419693+LoganHauser@users.noreply.github.com>
2022-10-27 14:47:19 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Create 3 objects
         CustomObject object1 = new CustomObject("1", 2, 3);
         CustomObject object2 = new CustomObject("4", 5, 6);
         CustomObject object3 = new CustomObject("7", 8, 9);
         
+        //Change attributes
         object1.attribute1 = "2";
         object2.attribute2 = 6;
         object3.attribute3 = 10;
 
-        object1.behavior()
-        object1.behavior()
-        object1.behavior()
+        //Use object behaviors
+        object1.behavior(3);
+        object1.behavior(7);
+        object1.behavior(11);
     }
 }
